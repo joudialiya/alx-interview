18fa1dc89e08d610c916de238eea141e01aba795
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-03 15:57:09 +0000

    Fix pep

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 PAscal Triangle
 """
 
+
 def pascal_triangle(n):
     """main function"""
     if n <= 0 or n is None:
@@ -12,13 +13,13 @@ def pascal_triangle(n):
     if n == 2:
         return [[1], [1, 1]]
     # the logic outside of the bondaries
-    current_list = []
+    curr_list = []
     prev_lists = pascal_triangle(n - 1)
     for p in range(0, n):
         if p == 0:
-            current_list.append(1)
+            curr_list.append(1)
         elif p == n - 1:
-            current_list.append(1)
+            curr_list.append(1)
         else:
-            current_list.append(prev_lists[n - 2][p] + prev_lists[n - 2][p - 1])
-    return [*prev_lists, current_list]
+            curr_list.append(prev_lists[n - 2][p] + prev_lists[n - 2][p - 1])
+    return [*prev_lists, curr_list]
