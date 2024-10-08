4545f03fd3ada573137bd167f1dd10b74d6900ec
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-08 13:09:42 +0000

    0.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,10 +8,11 @@ def canUnlockAll(boxes):
     """ the solution function """
     queue = [0]
     visited = set()
-    while (len(queue) > 0):
+    while len(queue) > 0:
         box = queue.pop(0)
         if box not in visited:
             visited.add(box)
             for keyTo in boxes[box]:
-                queue.append(keyTo)
+                if box < len(boxes):
+                    queue.append(keyTo)
     return len(visited) == len(boxes)
