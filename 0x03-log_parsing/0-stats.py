a268a0e04ff65243f4b5633ac03a65a0d5e4b93b
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-23 18:13:40 +0000

    0.

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,14 +20,18 @@ signal.signal(signal.SIGINT, output)
 if __name__ == '__main__':
     for line in sys.stdin:
         pattern = re.compile(
-            r"(?:\d{,3}\.?){4} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d*] " +
-            r"\"GET /projects/260 HTTP/1.1\" (\d{3}) (\d{3})"
+            r"(?:\d{,3}\.?){4} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] \"GET /projects/260 HTTP/1.1\" (.{3}) (\d+)"  # nopep8
         )
-        match = re.match(pattern, line)
+        line = line.strip()
+        match = re.fullmatch(pattern, line)
         if match:
             status, size = match.groups()[0], match.groups()[1]
-            status_count = status_dict.get(status)
-            status_dict[status] = status_count + 1 if status_count else 1
+            if status.isdecimal():
+                status_count = status_dict.get(status)
+                if status_count:
+                    status_dict[status] = status_count + 1
+                else:
+                    status_dict[status] = 1
             totale_size += int(size)
             line_counter += 1
 
