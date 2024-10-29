5a27af09ea1aea32b47e67309c0e19e062cbd10d
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-29 11:30:48 +0000

    0. rework the logic of the function, to not skip a char when we have a 1 byte char

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,33 +14,30 @@ def validUTF8(data):
         if state == NEW_CHAR:
             if (char & 0x80) == 0:
                 char_bytes_count = 1
-            elif (char & 0b11100000) == 0b11000000:
-                char_bytes_count = 2
-            elif (char & 0b11110000) == 0b11100000:
-                char_bytes_count = 3
-            elif (char & 0b11111000) == 0b11110000:
-                char_bytes_count = 4
+                char_byte_index = 0
             else:
-                # byte selection is not right
-                return False
-            char_byte_index = 0
-            state = FEACHING_CHAR
-        elif state == FEACHING_CHAR:
-            if char_bytes_count == 1:
-                # there is no check for the one byte char
-                state = NEW_CHAR
-                pass
-            else:
-                # for the multi byte chars each char should be
-                # a continuation byte
-                if char_byte_index < char_bytes_count - 1:
-                    # check for the continuation byte
-                    if (char & 0b11000000) != 0b10000000:
-                        return False
-                    char_byte_index += 1
+                if (char & 0b11100000) == 0b11000000:
+                    char_bytes_count = 2
+                elif (char & 0b11110000) == 0b11100000:
+                    char_bytes_count = 3
+                elif (char & 0b11111000) == 0b11110000:
+                    char_bytes_count = 4
                 else:
-                    state = NEW_CHAR
-    if char_bytes_count != char_byte_index:
+                    # byte selection is not right
+                    return False
+                char_byte_index = 0
+                state = FEACHING_CHAR
+        elif state == FEACHING_CHAR:
+            # for the multi byte chars each char should be
+            # a continuation byte
+            if char_byte_index < char_bytes_count - 1:
+                # check for the continuation byte
+                if (char & 0b11000000) != 0b10000000:
+                    return False
+                char_byte_index += 1
+            else:
+                state = NEW_CHAR
+    if char_bytes_count > 1 and char_bytes_count - 1 != char_byte_index:
         # the string ends before the end of a multi byte char
         return False
     return True
