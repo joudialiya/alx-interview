70c0eff93936b18624af976d754f6adc5962bc2e
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-14 10:35:01 +0000

    0. async need recursion

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
+const fetchChars = (characters, index) => {
+  if (index === characters.length) return;
+  request(characters[index], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      fetchChars(characters, index + 1);
+    }
+  });
+};
 
 request(
   `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
@@ -9,17 +20,7 @@ request(
       throw err;
     } else {
       const characters = JSON.parse(body).characters;
-      let i = 0;
-      while (i < characters.length) {
-        request(characters[i], (err, response, body) => {
-          if (err) {
-            throw err;
-          } else {
-            console.log(JSON.parse(body).name);
-            i = i + 1;
-          }
-        });
-      }
+      fetchChars(characters, 0);
     }
   }
 );
