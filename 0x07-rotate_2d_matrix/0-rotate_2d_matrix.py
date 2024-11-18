b548915eb136a978ebe1cc0d2e76772989b61987
Lagzal <mlagzal.sayhi@gmail.com>
2024-11-18 12:21:18 +0000

    0.

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+    """Rotate a matrix using geometry"""
+    rotated = []
+    n = len(matrix)
+    for y in range(0, n):
+        line = []
+        for x in range(0, n):
+            """
+            x' = xcosθ - ysinθ.
+            y' = xsinθ + ycosθ.
+            θ = pi / 2
+            """
+            rotated_x = x - (n - 1) / 2
+            rotated_y = y - (n - 1) / 2
+            original_x = rotated_y
+            original_y = - rotated_x
+            original_x += (n - 1) / 2
+            original_y += (n - 1) / 2
+            line.append(matrix[int(original_y)][int(original_x)])
+        rotated.append(line)
+    for y in range(0, n):
+        for x in range(0, n):
+            matrix[y][x] = rotated[y][x]
