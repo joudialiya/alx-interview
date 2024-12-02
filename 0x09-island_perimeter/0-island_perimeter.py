41c332d5b966696d3e4d81079d5c2658d2dd2845
Lagzal <mlagzal.sayhi@gmail.com>
2024-12-02 22:45:19 +0000

    0.

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Island perimeter"""
+from typing import List
+
+
+def island_perimeter(grid: List[List]):
+    """calculate island perimetre"""
+    perimeter = 0
+    h = len(grid)
+    w = len(grid[0])
+    for y in range(0, h):
+        # print(grid[y])
+        for x in range(0, w):
+            is_print = False
+            if x == 1 and y == 1:
+                is_print = True
+            if grid[y][x] == 1:
+                if x - 1 < 0 or grid[y][x - 1] == 0:
+                    perimeter += 1
+                if x + 1 > w or grid[y][x + 1] == 0:
+                    perimeter += 1
+                if y - 1 < 0 or grid[y - 1][x] == 0:
+                    perimeter += 1
+                if y + 1 > h or grid[y + 1][x] == 0:
+                    perimeter += 1
+    return perimeter
