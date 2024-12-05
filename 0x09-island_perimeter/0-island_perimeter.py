c93bf7a97513f6d896b12536b27e803dcf0ad3d0
Lagzal <mlagzal.sayhi@gmail.com>
2024-12-05 06:49:07 +0000

    0.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Island perimeter"""
-from typing import List
 
 
-def island_perimeter(grid: List[List]):
+def island_perimeter(grid):
     """calculate island perimetre"""
     perimeter = 0
     h = len(grid)
