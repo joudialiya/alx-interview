a1a804db08a7b538d2665e45a253b14a55c5df02
Lagzal <mlagzal.sayhi@gmail.com>
2024-12-05 06:50:33 +0000

    0.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,10 +13,10 @@ def island_perimeter(grid):
             if grid[y][x] == 1:
                 if x - 1 < 0 or grid[y][x - 1] == 0:
                     perimeter += 1
-                if x + 1 > w or grid[y][x + 1] == 0:
+                if x + 1 >= w or grid[y][x + 1] == 0:
                     perimeter += 1
                 if y - 1 < 0 or grid[y - 1][x] == 0:
                     perimeter += 1
-                if y + 1 > h or grid[y + 1][x] == 0:
+                if y + 1 >= h or grid[y + 1][x] == 0:
                     perimeter += 1
     return perimeter
